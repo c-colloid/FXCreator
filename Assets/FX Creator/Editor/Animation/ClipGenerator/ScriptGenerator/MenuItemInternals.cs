9a65a229908c248ff0192889aa1c29ed55828ff6
colloid <jack0904h3@yahoo.co.jp>
2024-07-26 00:02:22 +0900

    Add: ToggleClipScriptGenerator
    コンテキストメニューを追加するジェネレーターを作成

---
+++ Assets/FX Creator/Editor/Animation/ClipGenerator/ScriptGenerator/MenuItemInternals.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+static class MenuItemInternals
+{
+	public struct MenuItemInfo
+	{
+		public string Path { get; set; }
+		public bool IsSeparator { get; set; }
+		public int Priority { get; set; }
+	}
+	public static IEnumerable<MenuItemInfo> GetMenuItems(string menuPath, bool includeSeparators, bool localized)
+	{
+		if (s_GetMenuItems?.Invoke(null, new object[] { menuPath, includeSeparators, localized })
+			is not Array result)
+		{
+			Debug.LogError("(Editor) It could not get menu items. Please check the Unity version!");
+			return Enumerable.Empty<MenuItemInfo>();
+		}
+
+		return result.Cast<object>().Select(e =>
+		{
+			return new MenuItemInfo
+			{
+				Path = s_ScriptingMenuItem_path?.GetValue(e) as string,
+				IsSeparator = s_ScriptingMenuItem_isSeparator?.GetValue(e) as bool? ?? false,
+				Priority = s_ScriptingMenuItem_priority?.GetValue(e) as int? ?? 0
+			};
+		});
+	}
+
+	private readonly static MethodInfo s_GetMenuItems;
+	private readonly static PropertyInfo s_ScriptingMenuItem_path;
+	private readonly static PropertyInfo s_ScriptingMenuItem_priority;
+	private readonly static PropertyInfo s_ScriptingMenuItem_isSeparator;
+
+	static MenuItemInternals()
+	{
+		var eapm = Array.Empty<ParameterModifier>();
+		var eat = Array.Empty<Type>();
+		s_GetMenuItems = typeof(Menu).GetMethod("GetMenuItems", BindingFlags.Static | BindingFlags.NonPublic, null, new Type[] { typeof(string), typeof(bool), typeof(bool) }, eapm);
+		if (s_GetMenuItems == null) Debug.LogError("(Editor) s_GetMenuItems == null");
+
+		var scriptingMenuItemType = Type.GetType("UnityEditor.ScriptingMenuItem, UnityEditor.CoreModule");
+		if (scriptingMenuItemType == null) Debug.LogError("(Editor) scriptingMenuItemType == null");
+
+		s_ScriptingMenuItem_path = scriptingMenuItemType?.GetProperty("path", BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance, null, typeof(string), eat, eapm);
+		if (s_ScriptingMenuItem_path == null) Debug.LogError("(Editor) s_ScriptingMenuItem_path == null");
+
+		s_ScriptingMenuItem_isSeparator = scriptingMenuItemType?.GetProperty("isSeparator", BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance, null, typeof(bool), eat, eapm);
+		if (s_ScriptingMenuItem_isSeparator == null) Debug.LogError("(Editor) s_ScriptingMenuItem_isSeparator == null");
+
+		s_ScriptingMenuItem_priority = scriptingMenuItemType?.GetProperty("priority", BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance, null, typeof(int), eat, eapm);
+		if (s_ScriptingMenuItem_priority == null) Debug.LogError("(Editor) s_ScriptingMenuItem_priority == null");
+	}
+}
---
+++ Assets/FX Creator/Editor/Animation/ClipGenerator/ScriptGenerator/ToggleClipScriptGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using System.Linq;
+using System.IO;
+
+namespace colloid.FXCreator.Animation.Generator
+{
+	public class ToggleClipScriptGenerator : EditorWindow
+	{
+		private static string m_component = "Component";
+		private static string m_type = "Component";
+		
+		private static string CODE = 
+$@"using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace colloid.FXCreator.Animation
+{{
+	public class ToggleClip_#m_component# : ToggleClip
+	{{
+		const string m_componentMenuItem = m_componentMenuItemPath + nameof(#m_type#);
+	
+		[MenuItem(m_menuItem ,validate = true ,menuItem = m_componentMenuItem)]
+		static bool VaridationGenerate#m_component#Clip()
+		{{
+			return Selection.gameObjects.Any()
+				&& Selection.gameObjects.All(o => o.TryGetComponent<#m_type#>(out var result));
+		}}
+		
+		[MenuItem(m_menuItem ,menuItem = m_componentMenuItem, priority = 1011)]
+		static void Generate#m_component#Clip()
+		{{
+			GenerateToggelActiveComponentsClip(Selection.gameObjects.Select(o => o.GetComponent<#m_type#>()));
+		}}
+	}}
+}}";
+	
+		private static string METHOD =
+$@"const string m_componentMenuItem = m_componentMenuItemPath + nameof(#m_type#);
+
+[MenuItem(m_menuItem ,validate = true ,menuItem = m_componentMenuItem)]
+static bool VaridationGenerate#m_component#Clip()
+{{
+	return Selection.gameObjects.Any()
+		&& Selection.gameObjects.All(o => o.TryGetComponent<#m_type#>(out var result));
+}}
+
+[MenuItem(m_menuItem ,menuItem = m_componentMenuItem, priority = 1011)]
+static void Generate#m_component#Clip()
+{{
+	GenerateToggelActiveComponentsClip(Selection.gameObjects.Select(o => o.GetComponent<#m_type#>()));
+}}";
+	
+		[MenuItem("Tools/ClipGen/Settings/GenerateScript")]
+		static void GenerateScript()
+		{
+			var window = EditorWindow.CreateInstance<ToggleClipScriptGenerator>();
+			window.title = "GenerateScript";
+			window.ShowAuxWindow();
+		}
+		
+		void CreateGUI()
+		{
+			var root = rootVisualElement;
+			var dropdown = new DropdownField(){value = "select Component",style = {flexGrow = 1}};
+			dropdown.choices = MenuItemInternals.GetMenuItems(nameof(Component),false,false).Select(o => o.Path).ToList();
+			dropdown.RegisterValueChangedCallback(evt =>{
+				var path = evt.newValue;
+				m_component = path.Split("/").Last().Replace(" ","");
+				m_type = m_component;
+			});
+			
+			root.Add(dropdown);
+			
+			root.Add(new VisualElement{style = {flexGrow = 2}});
+			
+			var buttonsBox = new VisualElement(){style = {flexDirection = FlexDirection.RowReverse, flexGrow = 1}};
+			
+			root.Add(buttonsBox);
+			
+			var ok = new Button(){text = "OK"};
+			ok.clicked += () => {
+				if (dropdown.index < 0) return;
+				var code = CODE.Replace("#m_component#",m_component).Replace("#m_type#",m_type);
+				
+				// 作成するアセットのパス
+				var filePath = $"Assets/FX Creator/Editor/Animation/ClipGenerator/Gen/ToggleClip_{m_component}.cs";
+
+				// もし名前(パス)が重複していた場合に、自動で語尾に「Sample1.cs」みたく数字をつけてくれる
+				var assetPath = AssetDatabase.GenerateUniqueAssetPath(filePath);
+
+				// アセット(.cs)を作成する
+				File.WriteAllText(filePath, code);
+        
+				// 変更があったアセットをインポートする(UnityEditorの更新)
+				AssetDatabase.Refresh();
+			};
+			
+			buttonsBox.Add(ok);
+			
+			var copy = new Button(){text = "Copy"};
+			copy.clicked += () => {
+				var method = METHOD.Replace("#m_component#",m_component).Replace("#m_type#",m_type);
+				
+				EditorGUIUtility.systemCopyBuffer = method;
+			};
+			
+			buttonsBox.Add(copy);
+			
+			var cansel = new Button(){text = "Cancel"};
+			cansel.clicked += () => this.Close();
+			
+			buttonsBox.Add(cansel);
+		}
+	}
+}
