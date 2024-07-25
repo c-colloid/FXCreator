bac1d3ddff6487912b9ac1c74506d52cd89a3004
colloid <jack0904h3@yahoo.co.jp>
2024-07-26 00:06:35 +0900

    Add: Create AnimatorCreatorData

---
+++ Assets/FX Creator/Editor/Animator/AnimatorCreatorData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+
+[CreateAssetMenu(menuName = "FXCreator/AniamatorCreatorData")]
+public class AnimatorCreatorData : ScriptableObject
+{
+    public List<NodeData> nodeData_list;
+	public List<EdgeData> edgeData_list;
+}
+
+[Serializable]
+public class NodeData
+{
+    public string uid;
+    public string nodeType_str;
+	public Type nodeType { get { return Type.GetType(nodeType_str); } }
+    public Rect localBound;
+}
+
+[Serializable]
+public class EdgeData
+{
+    public string uid_outputNode;
+    public string uid_outputPort;
+    public string uid_inputNode;
+    public string uid_inputPort;
+}
